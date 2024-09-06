451e47b1b233d9e8f1039df03b883d9b99d8c252
hatimlechgar <h.lechgar@gmail.com>
2024-09-06 16:42:52 +0200

    read port from process.env.PORT

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
