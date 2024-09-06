44d09bab10fb4275772713620b042327bacf3569
hatimlechgar <h.lechgar@gmail.com>
2024-09-06 16:49:24 +0200

    add consultants end point

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+
+app.get('/consultants', (req, res) => {
+  const consultants = [
+    { id: 1, firstName: 'John', lastName: 'Doe', CIN: 'A123456' },
+    { id: 2, firstName: 'Jane', lastName: 'Smith', CIN: 'B654321' },
+    { id: 3, firstName: 'Sam', lastName: 'Green', CIN: 'C987654' },
+  ];
+  res.json(consultants);
+});
+
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
