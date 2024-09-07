4c71a746f683462d469fbcf4d630575adfa50e76
FaroukElorfi <elorfi.farouk@gmail.com>
2024-09-07 15:51:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/consultants', (req, res) => {
+  const consultants = [
+    { id: 1, firstName: 'Ahmed', lastName: 'El Haddad', CIN: 'A123456' },
+    { id: 2, firstName: 'Fatima', lastName: 'Ben Ali', CIN: 'B654321' }
+  ];
+  res.json(consultants);
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server running');
 });
-
