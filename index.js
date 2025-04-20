be00e00e4c9c60a3c69765c02149e0119ec9ae82
RawnaqAsbai <rawnaq.asbai@gmail.com>
2025-04-20 15:43:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/consultants', (req, res) => {
+app.get('/syllabus', (req, res) => {
   const syllabus = [
     {
       id: 1,
