45f6b2e351e5568323bcb8b2287573e9f8658d3f
hichamterjaoui <hicham.terjaoui.emc2@ehtp.ac.ma>
2025-04-25 23:35:23 +0100

    Update fes.js

--- api/villes/fes.js
+++ api/villes/fes.js
@@ -1,1 +1,7 @@
 
+// api/ville/fes.js
+module.exports = {
+  name: "Fès",
+  description: "Ville des merveilles, Fès est un joyau historique aux ruelles authentiques.",
+  image: "https://yourdomain.com/images/fes.jpg"
+};
