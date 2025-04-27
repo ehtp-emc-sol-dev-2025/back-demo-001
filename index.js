d1846df806ed57719be229af3970d3bd3a8567bf
hichamterjaoui <hicham.terjaoui.emc2@ehtp.ac.ma>
2025-04-27 15:59:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+
 const express = require('express');
 const app = express();
-const port = 3000;
-
-// Importer les données de Fès
-const fesData = require('./api/ville/fes');
-
-// Endpoint pour renvoyer les données de la ville de Fès
-app.get('/api/ville/fes', (req, res) => {
-  res.json(fesData);
+//comment
+app.get('/healthcheck', (req, res) => {
+  res.status(200).json({
+    status: 'UP',
+    timestamp: new Date().toISOString(),
+    version: '1.0.0'
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Serveur en écoute sur le port ${port}`);
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server running');
 });
